859b32d493a94278c1e631e22cea3d76566569a2
Colin Whiteford <colinwhiteford516@gmail.com>
2025-09-24 19:27:00 -0400

    Started Work on Week 6 Project

--- Spritekit-Actions/Spritekit Actions/GameScene.swift
+++ Spritekit-Actions/Spritekit Actions/GameScene.swift
@@ -1,8 +1,8 @@
 //
 //  GameScene.swift
-//  Whack a Mole
+//  Sprite Actions
 //
-//  Created by Colin Whiteford on 9/17/25.
+//  Created by Colin Whiteford on 9/24/25.
 //
 
 import SpriteKit
@@ -12,40 +12,42 @@ class GameScene: SKScene {
     
     var lastUpdateTime: TimeInterval = 0
     var deltaTime: TimeInterval = 0
-    let zombie = SKSpriteNode(imageNamed: "zombie1")
-    let zombieScale = 0.6
-    var zombieVelocity: CGPoint = CGPoint.zero
-    var zombieSpeed: CGFloat = 480
-    let enemyCollisionSound : SKAction = SKAction.playSoundFileNamed("hitCatLady", waitForCompletion: false)
-    func spawnEnemy()
+    class ActionableSprite
     {
-        let enemy = SKSpriteNode(imageNamed: "enemy")
-        enemy.name = "enemy"
-        enemy.setScale(zombieScale)
-        enemy.position = CGPoint(x: size.width / 2 + enemy.size.width/2, y:0)
-        addChild(enemy)
-        let actionMove = SKAction.moveTo(x: -size.width/2 - enemy.size.width/2, duration: 2.0)
-        let actionRemove = SKAction.removeFromParent()
-        let enemySequence = SKAction.sequence([actionMove, actionRemove])
-        enemy.run(enemySequence)
+        init(sprite: SKSpriteNode, position: CGPoint, scale: CGFloat, action: SKAction, scene: SKScene)
+        {
+            self.sprite = sprite
+            self.sprite.position = position
+            self.sprite.setScale(scale)
+            scene.addChild(sprite)
+            self.action = SKAction.sequence([SKAction.run {self.UpdateInteractionState(newState: false)}, action, SKAction.run {self.UpdateInteractionState(newState: true)}])
+        }
+        var sprite: SKSpriteNode
+        var action: SKAction = SKAction()
+        var canBeInteractedWith: Bool = true
+        func UpdateInteractionState(newState: Bool)
+        {
+            canBeInteractedWith = newState
+        }
+        func CheckTouchAndRunAction(touchLocation: CGPoint)
+        {
+            if(!canBeInteractedWith){return;}
+            if(!sprite.frame.contains(touchLocation)){return;}
+            sprite.run(action)
+        }
     }
     
+    var interactiveSprites: [ActionableSprite] = []
+
     
     //Awake
     override func didMove(to view: SKView)
     {
         backgroundColor = SKColor.black
-        zombie.position = CGPoint(x: -size.width / 4, y: -size.height / 4)
-        zombie.setScale(zombieScale)
-        addChild(zombie)
-        let wait = SKAction.wait(forDuration: 2.0)
-        let spawnEnemyAction = SKAction.run {
-            [weak self] in self?.spawnEnemy()
-        }
-        let spawnEnemySequence = SKAction.sequence([spawnEnemyAction, wait])
-        let keepRespawningEnemy = SKAction.repeatForever(spawnEnemySequence)
-        run(keepRespawningEnemy)
-    
+        //let scaleDownAction: SKAction = SKAction.scale(by: 0.5, duration: 2)
+        //interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 0, y: 0), scale: CGFloat(0.5), action: scaleDownAction, scene: self))
+        let scaleUpAndDown: SKAction = SKAction.sequence([SKAction.scale(by: 2, duration: 0.5), SKAction.scale(by: 0.5, duration: 0.5)])
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 0, y: 0), scale: CGFloat(0.5), action: scaleUpAndDown, scene: self))
     }
     //Update
     override func update(_ currentTime: TimeInterval)
@@ -58,87 +60,17 @@ class GameScene: SKScene {
         {
             deltaTime = 0
         }
-        lastUpdateTime = currentTime
-        Move(sprite: zombie, velocity: zombieVelocity)
-        checkZombieBounds()
-        rotateZombie(sprite: zombie, direction: zombieVelocity)
-        checkCollision()
     }
-    func DetermineVelocity(location: CGPoint)
-    {
-        let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
-        let length = sqrt(offset.x * offset.x + offset.y * offset.y)
-        if length > 0
-        {
-            let direction = CGPoint(x: offset.x / length, y: offset.y / length)
-            zombieVelocity = CGPoint(x: direction.x * zombieSpeed, y: direction.y * zombieSpeed)
-        }
-    }
-    func Move( sprite: SKSpriteNode, velocity: CGPoint)
-    {
-        let amountToMove = CGPoint(x: velocity.x * deltaTime, y: velocity.y * deltaTime)
-        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
-    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
         guard let touch = touches.first else{return}
         let touchLocation = touch.location(in: self)
-        DetermineVelocity(location: touchLocation)
-    }
-    //Move Finger
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-        guard let touch = touches.first else{return}
-        let touchLocation = touch.location(in: self)
-        DetermineVelocity(location: touchLocation)
-    }
-    func checkZombieBounds()
-    {
-        let bottomLeft = CGPoint(x: -size.width / 2, y:-size.height / 2)
-        let topRight = CGPoint(x: size.width / 2, y: size.height / 2)
-        if zombie.position.x <= bottomLeft.x
-        {
-            zombie.position.x = bottomLeft.x
-            zombieVelocity.x = -zombieVelocity.x
-        }
-        if zombie.position.x >= topRight.x
-        {
-            zombie.position.x = topRight.x
-            zombieVelocity.x = -zombieVelocity.x
-        }
-        if zombie.position.y <= bottomLeft.y
-        {
-            zombie.position.y = bottomLeft.y
-            zombieVelocity.y = -zombieVelocity.y
-        }
-        if zombie.position.y >= topRight.y
-        {
-            zombie.position.y = topRight.y
-            zombieVelocity.y = -zombieVelocity.y
+        for interactiveSprite in interactiveSprites {
+            interactiveSprite.CheckTouchAndRunAction(touchLocation: touchLocation)
         }
     }
-    func rotateZombie(sprite: SKSpriteNode, direction: CGPoint)
-    {
-        sprite.zRotation = atan2(direction.y, direction.x)
-    }
-    func checkCollision()
-    {
-        var hitEnemies : [SKSpriteNode] = []
-        enumerateChildNodes(withName: "enemy")
-        {
-            (node, _) in
-            let enemyNode = node as! SKSpriteNode
-            if enemyNode.frame.intersects(self.zombie.frame)
-            {
-                hitEnemies.append(enemyNode)
-            }
-        }
-        for enemy in hitEnemies
-        {
-            enemy.removeFromParent()
-            run(enemyCollisionSound)
-            print("Hit an enemy!")
-        }
-    }
+
+
     
 }
