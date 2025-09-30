e52ee95bbdd175a0d3f43ff2afca8fa513100bdb
Colin Whiteford <colinwhiiteford516@gmail.com>
2025-09-30 15:37:29 -0400

    Finished All Actions

--- Spritekit-Actions/Spritekit Actions/GameScene.swift
+++ Spritekit-Actions/Spritekit Actions/GameScene.swift
@@ -44,10 +44,29 @@ class GameScene: SKScene {
     override func didMove(to view: SKView)
     {
         backgroundColor = SKColor.black
-        //let scaleDownAction: SKAction = SKAction.scale(by: 0.5, duration: 2)
-        //interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 0, y: 0), scale: CGFloat(0.5), action: scaleDownAction, scene: self))
+        let scaleDownAction: SKAction = SKAction.scale(by: 0.5, duration: 2)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: -50, y: 0), scale: CGFloat(0.5), action: scaleDownAction, scene: self))
         let scaleUpAndDown: SKAction = SKAction.sequence([SKAction.scale(by: 2, duration: 0.5), SKAction.scale(by: 0.5, duration: 0.5)])
-        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 0, y: 0), scale: CGFloat(0.5), action: scaleUpAndDown, scene: self))
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 50, y: 0), scale: CGFloat(0.5), action: scaleUpAndDown, scene: self))
+        let moveUp: SKAction = SKAction.moveBy(x: 0, y: 50, duration: 1)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 150, y: 0), scale: CGFloat(0.5), action: moveUp, scene: self))
+        let moveUpAndDown: SKAction = SKAction.sequence([SKAction.moveBy(x: 0, y: 50, duration: 1), SKAction.moveBy(x: 0, y: -50, duration: 1)])
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 250, y: 0), scale: CGFloat(0.5), action: moveUpAndDown, scene: self))
+        let removeFromParent: SKAction = SKAction.removeFromParent()
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 350, y: 0), scale: CGFloat(0.5), action: removeFromParent, scene: self))
+        let newTexture = SKTexture(imageNamed: "zombie4")
+        let changeTexture: SKAction = SKAction.setTexture(newTexture)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: 450, y: 0), scale: CGFloat(0.5), action: changeTexture, scene: self))
+        let fadeAlpha: SKAction = SKAction.fadeAlpha(to: 0, duration: 1)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: -150, y: 0), scale: CGFloat(0.5), action: fadeAlpha, scene: self))
+        let rotateOnce: SKAction = SKAction.rotate(byAngle: CGFloat.pi * 2, duration: 1)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: -250, y: 0), scale: CGFloat(0.5), action: rotateOnce, scene: self))
+        let rotateThreeTimes: SKAction = SKAction.rotate(byAngle: CGFloat.pi * 6, duration: 3)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: -350, y: 0), scale: CGFloat(0.5), action: rotateThreeTimes, scene: self))
+        let playSound: SKAction = SKAction.playSoundFileNamed("win", waitForCompletion: false)
+        interactiveSprites.append(ActionableSprite(sprite: SKSpriteNode(imageNamed: "zombie1"), position: CGPoint(x: -450, y: 0), scale: CGFloat(0.5), action: playSound, scene: self))
+        
+        
     }
     //Update
     override func update(_ currentTime: TimeInterval)
